d38b248d0cbad05365e3fd6966f942554f078238
Okalisteur <tsantarandrianarison@gmail.com>
2025-07-14 22:50:06 +0300

    Allowing CORS

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*' );
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS');
+  next();
+});
 
 // --- Création du graphe à partir du format Cytoscape
 function parseCytoscapeInput(data) {
